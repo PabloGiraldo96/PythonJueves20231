79247eb309d7feada8f7024ab36bd596c6b65c6c
Juan Pablo Jaramillo Giraldo <82364328+HotSauce96@users.noreply.github.com>
2023-03-11 15:27:12 -0500

    Create ejercicioCiudades.py
    Ejercicios de reversar listas

---
+++ ejercicioCiudades.py
@@ -0,0 +1,12 @@
+#Ejercicios de reversar listas
+
+ciudades = ['medellin', 'cartagena', 'bucaramanga', 'cali', 'bogota', 'pereira', 'popayan', 'leticia']
+
+ciudades.reverse()
+print('Ciudades reversa:', ciudades)
+
+animales = ['perros', 'gatos', 'gatos', 'conejos', 'gallinas', 'vacas']
+
+animales.reverse()
+print (animales)
+
