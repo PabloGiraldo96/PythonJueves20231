7464f96ed3ea90559f8e1ca5ca011b3cc14822c5
Juan Pablo Jaramillo Giraldo <82364328+HotSauce96@users.noreply.github.com>
2023-03-20 14:31:48 -0500

    Se modifica el ejemplo.
    Se añaden los retornos del método input; de esta manera el usuario puede ingresar los números de la lista (20) y luego compararlos

--- NumeroaBuscar.py
+++ NumeroaBuscar.py
@@ -1,14 +1,25 @@
-Lista = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+#Lista vacía que se llenará con un ciclo for
 
-print(f"La lista es: {Lista}")
-numeroaBuscar = int(input("Ingrese un número para buscarlo dentro de la lista"))
-seEncontro = False    
-    
-for i in Lista:
+lista =[]
+
+#Variable booleana que indicará si encontramos el número dentro de la lista (SI / NO)
+seEncontro = False
+
+#Ciclo con las 20 vueltas como se pide, y se crea la variable numeros adentro, esta variable es la que va a llenar la lista; con el método .append ingresamos la variable a la lista.
+for i in range(0, 20):
+    numeros = int(input("Ingrese numero: "))
+    lista.append(numeros)
+
+#Ya por fuera del ciclo creamos la variable numeroaBuscar, que se va a comparar con el iterador i y sí cumple la condición entonces la variable seEncontro se vuelve verdadera.
+#Luego se imprimen los mensajes si se encontró o no.
+
+numeroaBuscar=int(input("Ingrese numero para ver si está en la lista: "))
+for i in lista:
     if i == numeroaBuscar:
         seEncontro = True
         break
 if seEncontro:
-    print(f"el número {numeroaBuscar} se encontró dentro de la lista")
-else :
-    print(f"el numero {numeroaBuscar} no se encontró")
+    print(f"El numero que ingreso {numeroaBuscar} se encuentra dentro de la lista: {lista}")
+else:
+    print(f"El numero que ingreso {numeroaBuscar} no se encuentra dentro de la lista: {lista}")
+
